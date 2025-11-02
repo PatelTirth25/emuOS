c3aa4933292dd28a9689073842f4eb7293f0b223
PatelTirth25 <tirthpatel204@gmail.com>
2025-11-02 20:46:30 +0530

    feat: serial monitor added

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,35 +2,38 @@
 #![no_main]
 
 mod boot;
+mod framebuffer;
+mod serial;
 
-use boot::boot_info;
 use x86_64::instructions::hlt;
 
 fn main() -> ! {
-    let boot_info = boot_info();
-    let framebuffer = &boot_info.framebuffer;
-    let width = framebuffer.width();
-    let height = framebuffer.height();
+    serial_println!("emuOS!");
 
-    for i in 0..height {
-        for j in 0..width {
-            let color: u32 = if (i == 0 || i == height - 1) || (j == 0 || j == width - 1) {
-                0xFFFF0000
-            } else {
-                0xFFFFFFFF
-            };
+    // let boot_info = boot_info();
+    // let framebuffer = &boot_info.framebuffer;
+    // let width = framebuffer.width();
+    // let height = framebuffer.height();
 
-            let pixel_offset = i * framebuffer.pitch() + j * 4;
-
-            unsafe {
-                framebuffer
-                    .addr()
-                    .add(pixel_offset as usize)
-                    .cast::<u32>()
-                    .write(color)
-            };
-        }
-    }
+    // for i in 0..height {
+    //     for j in 0..width {
+    //         let color: u32 = if (i == 0 || i == height - 1) || (j == 0 || j == width - 1) {
+    //             0xFFFF0000
+    //         } else {
+    //             0xFFFFFFFF
+    //         };
+    //
+    //         let pixel_offset = i * framebuffer.pitch() + j * 4;
+    //
+    //         unsafe {
+    //             framebuffer
+    //                 .addr()
+    //                 .add(pixel_offset as usize)
+    //                 .cast::<u32>()
+    //                 .write(color)
+    //         };
+    //     }
+    // }
 
     loop {
         hlt();
