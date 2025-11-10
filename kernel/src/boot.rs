79feeabbba6377fc2769fd88cc542751256716da
PatelTirth25 <tirthpatel204@gmail.com>
2025-11-10 23:37:16 +0530

    feat: heap memory added

--- kernel/src/boot.rs
+++ kernel/src/boot.rs
@@ -1,8 +1,10 @@
 use limine::framebuffer::Framebuffer;
+use limine::response::MemoryMapResponse;
 use limine::BaseRevision;
-use limine::request::{FramebufferRequest, RequestsEndMarker, RequestsStartMarker};
+use limine::request::{FramebufferRequest, HhdmRequest, MemoryMapRequest, RequestsEndMarker, RequestsStartMarker};
 use spin::Once;
-
+use crate::memory::heap::init_heap;
+use crate::memory::{self, BootInfoFrameAllocator};
 use crate::{gdt, interrupt, main};
 
 /// Sets the base revision to the latest revision supported by the crate.
@@ -17,6 +19,14 @@ pub static BASE_REVISION: BaseRevision = BaseRevision::new();
 #[unsafe(link_section = ".requests")]
 pub static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
 
+#[used]
+#[unsafe(link_section = ".requests")]
+static MEMORY_MAP_REQUEST: MemoryMapRequest = MemoryMapRequest::new();
+
+#[used]
+#[unsafe(link_section = ".requests")]
+static HHDM_REQUEST: HhdmRequest = HhdmRequest::new();
+
 /// Define the start and end markers for Limine requests.
 #[used]
 #[unsafe(link_section = ".requests_start_marker")]
@@ -27,6 +37,7 @@ static _END_MARKER: RequestsEndMarker = RequestsEndMarker::new();
 
 pub struct BootInfo {
     pub framebuffer: Framebuffer<'static>,
+    pub memory_map: &'static MemoryMapResponse,
 }
 
 static BOOT_INFO: Once<BootInfo> = Once::new();
@@ -63,7 +74,9 @@ unsafe extern "C" fn kmain() -> ! {
         .next()
         .expect("No framebuffer available");
 
-    let boot_info = BootInfo { framebuffer };
+    let memory_map = MEMORY_MAP_REQUEST.get_response().expect("No memory map available");
+
+    let boot_info = BootInfo { framebuffer,memory_map };
     BOOT_INFO.call_once(|| boot_info);
     init();
 
@@ -72,6 +85,7 @@ unsafe extern "C" fn kmain() -> ! {
 }
 
 pub fn init() {
+    use x86_64::VirtAddr;
     gdt::init();
     interrupt::init_idt();
 
@@ -81,4 +95,12 @@ pub fn init() {
         pics.write_masks(0, 0);
     }
     x86_64::instructions::interrupts::enable();
+
+    let hhdm = HHDM_REQUEST.get_response().expect("no HHDM");
+    let phys_offset = VirtAddr::new(hhdm.offset());
+
+    let mut mapper = unsafe { memory::init(phys_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(boot_info().memory_map) };
+
+    init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 }
